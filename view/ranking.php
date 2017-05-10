91aa59e162170baa6b4809d85605e4bb6bebe297
Ibris <lucianotv12@gmail.com>
2017-05-10 19:12:48 -0300

    s

--- view/ranking.php
+++ view/ranking.php
@@ -3,11 +3,11 @@
       <!-- Modal -->
       <div class="modal-dialog ranking bold uppercase">
         <!-- Modal content-->
-        <div class="modal-content mlb-modal">
+        <div class="modal-content ">
           <div class="modal-body">
             <img class="ranking-text" src="<?php echo IMGS?>ka/ranking-text.png">
             <img class="ranking-subtext" src="<?php echo IMGS?>ka/ranking-subtext.png">
-            <p style="padding-top: 15px; font-size: 40px;">¡No te frenes! ¡Si tus Puntos de Venta Ganan, Vos También podés ganar! Próximamente publicaremos resultados.</p>
+            <p style="padding-top: 15px; font-size: 30px;">¡No te frenes! ¡Si tus Puntos de Venta Ganan, Vos También podés ganar! Próximamente publicaremos resultados.</p>
           </div> 
         </div>
       </div>     
