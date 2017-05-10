36a6410efca3f5bd2fd8cd6cc0de6aecffba6649
Ibris <lucianotv12@gmail.com>
2017-05-10 18:33:39 -0300

    gi

--- view/volumen_compra.php
+++ view/volumen_compra.php
@@ -5,6 +5,7 @@
         <div class="modal-content mlb-modal box-1">
           <div class="modal-body">
             <img src="<?php echo IMGS?>ka/superen-volumen.png">
+<!--            <img src="<?php echo IMGS?>gif/superen-volumen.png">-->
           </div>
           <a class="back-home" href="./home.html">
             <img src="<?php echo IMGS?>ka/arrow-left.png">
