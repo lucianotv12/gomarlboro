c6c2db2759a59140de539b7688a60f2eaaa0ca96
Ibris <lucianotv12@gmail.com>
2017-06-07 10:27:58 -0300

    s

--- view/ranking-locales-a.php
+++ view/ranking-locales-a.php
@@ -5,31 +5,48 @@
           <div class="modal-body">
             <img class="ranking-text" src="<?php echo IMGS?>ka/ranking-text.png">
             <img class="ranking-subtext" src="<?php echo IMGS?>ka/ranking-subtext2.png">
+             <table class="table table-responsive" >
+              <tr><td colspan="8" style="text-align: center;">ACUMULADO SEMANA 3</td></tr>
+
+                <tr>
+                  <td width="120px" >Supervisor</td>
+                  <td width="120px" >Direccion</td>
+                  <td>Compras <br/> f.mlb</td>
+                  <td>Faltante <br/> OB</td>
+                  <td>Ventas <br/> f.mlb</td>
+                  <td>Faltante <br/> OB</td>                      
+                  <td>Compras <br/>core</td>
+                  <td>Faltante <br/> OB</td>
+                  <td>Cupones</td>
+                </tr>
+            </table>              
             <div class="ranking-container">
 
                   <table class="table table-responsive" >
-                    <tr><td colspan="8" style="text-align: center;">SEMANA 3</td></tr>
-                    <tr>
-                      <td>Supervisor</td>
-                      <td>Direccion</td>
-                      <td>Compras <br/> f.mlb</td>
-                      <td>Faltante <br/> OB</td>
-                      <td>Ventas <br/> f.mlb</td>
-                      <td>Faltante <br/> OB</td>                      
-                      <td>Compras <br/>core</td>
-                      <td>Faltante <br/> OB</td>
-                      <td>Ventas <br/>core</td>
-                      <td>Faltante <br/> OB</td>
-
-                      <td>Cupones</td>
-                    </tr>
 
 
-              <?php foreach($pdvs as $pdv): ?>     
+
+              <?php foreach($pdvs as $pdv): 
+                if(strlen($pdv["supervisor"]) > 22):
+                 $pdv["supervisor"] = substr($pdv["supervisor"], 0, 19) . "...";
+                endif;
+                if(strlen($pdv["direccion"]) > 22):
+                 $pdv["direccion"] = substr($pdv["direccion"], 0, 19) . "...";
+                endif;                
+                if($pdv["compras_flia_mlb"] < 0): $pdv["compras_flia_mlb"] = 0; endif;
+                if($pdv["faltantes_flia_mlb"] < 0): $pdv["faltantes_flia_mlb"] = 0; endif;
+                if($pdv["ventas_flia_mlb"] < 0): $pdv["ventas_flia_mlb"] = 0; endif;
+                if($pdv["faltantes_ventas"] < 0): $pdv["faltantes_ventas"] = 0; endif;
+                if($pdv["compras_core"] < 0): $pdv["compras_core"] = 0; endif;
+                if($pdv["faltantes_core"] < 0): $pdv["faltantes_core"] = 0; endif;
+                if($pdv["ventas_core"] < 0): $pdv["ventas_core"] = 0; endif;
+                if($pdv["faltantes_ventas_core"] < 0): $pdv["faltantes_ventas_core"] = 0; endif;
+
+              ?>     
        
-                    <tr style="font-size: 12px">
-                      <td  ><?php echo utf8_encode(addslashes($pdv["supervisor"]))?></td>
-                      <td ><?php echo utf8_encode(addslashes($pdv["direccion"]))?></td>
+                    <tr style="font-size: 12px; text-align: left;">
+                      <td  width="120px" ><?php echo utf8_encode(addslashes($pdv["supervisor"]))?></td>
+                      <td  width="120px"><?php echo utf8_encode(addslashes($pdv["direccion"]))?></td>
                       <td ><?php echo $pdv["compras_flia_mlb"]?></td>
                       <td ><?php echo $pdv["faltantes_flia_mlb"]?></td>
                       <td ><?php echo $pdv["ventas_flia_mlb"]?></td>
--- view/ranking-locales-b.php
+++ view/ranking-locales-b.php
@@ -5,27 +5,40 @@
           <div class="modal-body">
             <img class="ranking-text" src="<?php echo IMGS?>ka/ranking-text.png">
             <img class="ranking-subtext" src="<?php echo IMGS?>ka/ranking-subtext2.png">
+ 
+             <table class="table table-responsive" >
+              <tr><td colspan="8" style="text-align: center;">ACUMULADO SEMANA 3</td></tr>
+
+              <tr style="text-align: left">
+                <td width="160px">Supervisor</td>
+                <td width="160px">Direccion</td>
+                <td>Compras <br/> flia mlb</td>
+                <td>Faltante <br/> ob</td>
+                <td>Compras <br/>core</td>
+                <td>Faltante <br/> ob</td>
+                <td>Cupones</td>
+
+              </tr>
+            </table>  
             <div class="ranking-container">
                   <table class="table table-responsive" >
-                    <tr><td colspan="8" style="text-align: center;">SEMANA 3</td></tr>
+              <?php foreach($pdvs as $pdv): 
+                if(strlen($pdv["supervisor"]) > 30):
+                 $pdv["supervisor"] = substr($pdv["supervisor"], 0, 27) . "...";
+                endif;
+                if(strlen($pdv["direccion"]) > 30):
+                 $pdv["direccion"] = substr($pdv["direccion"], 0, 27) . "...";
+                endif;                
+                if($pdv["compras_flia_mlb"] < 0): $pdv["compras_flia_mlb"] = 0; endif;
+                if($pdv["faltantes_flia_mlb"] < 0): $pdv["faltantes_flia_mlb"] = 0; endif;
+                if($pdv["compras_core"] < 0): $pdv["compras_core"] = 0; endif;
+                if($pdv["faltantes_core"] < 0): $pdv["faltantes_core"] = 0; endif;
 
-                    <tr>
-                      <td>Supervisor</td>
-                      <td>Direccion</td>
-                      <td>Compras <br/> flia mlb</td>
-                      <td>Faltante <br/> ob</td>
-                      <td>Compras <br/>core</td>
-                      <td>Faltante <br/> ob</td>
-                      <td>Cupones</td>
-
-                    </tr>
-
-
-              <?php foreach($pdvs as $pdv): ?>     
+              ?>     
        
-                    <tr style="font-size: 12px">
-                      <td  ><?php echo utf8_encode(addslashes($pdv["supervisor"]))?></td>
-                      <td ><?php echo utf8_encode(addslashes($pdv["direccion"]))?></td>
+                    <tr style="font-size: 12px; text-align: left">
+                      <td width="160px" ><?php echo utf8_encode(addslashes($pdv["supervisor"]))?></td>
+                      <td width="160px"><?php echo utf8_encode(addslashes($pdv["direccion"]))?></td>
                       <td ><?php echo $pdv["compras_flia_mlb"]?></td>
                       <td ><?php echo $pdv["faltantes_flia_mlb"]?></td>
                       <td ><?php echo $pdv["compras_core"]?></td>
