d33df1e3fbb1ff6748319b34190fe17d5e419b9a
Ibris <lucianotv12@gmail.com>
2017-06-09 17:53:07 -0300

    MINIZ

--- view/carga_ezd.php
+++ view/carga_ezd.php
@@ -17,8 +17,14 @@ $idProvincia = $_GET["code"];
 $_provincias=User_gt::get_ezd($idProvincia,$_usuario->usuario);	
 
 foreach($_provincias as $_provincia):
+	if($_provincia["desc_EZD"] == 'CASA MUNIZ S.A.'):
+		$muestra = 'CASA MU&Ntilde;IZ S.A.';
+	else:
+		$muestra = $_provincia["desc_EZD"];
+
+	endif;	
 ?>
-	<option value="<?php echo $_provincia["desc_EZD"]?>"><?php echo $_provincia["desc_EZD"]?></option>
+	<option value="<?php echo utf8_encode($_provincia["desc_EZD"])?>"><?php echo $muestra?></option>
 <?php endforeach;?>
 
 
