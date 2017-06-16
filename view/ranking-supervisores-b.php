efde1ef8d0d4d02cbff9f8b1487f3cd869ed72a5
Ibris <lucianotv12@gmail.com>
2017-06-16 15:01:25 -0300

    s

--- view/ranking-supervisores-b.php
+++ view/ranking-supervisores-b.php
@@ -18,14 +18,16 @@
                     <td>¿GANA?</td>
 
                   </tr>                  
-              <?php foreach($supervisores as $supervisor): ?>     
+              <?php foreach($supervisores as $supervisor): 
+                if(!$supervisor["locales_ganador_gc"]) $supervisor["locales_ganador_gc"] = 0;
+              ?>     
        
                     <tr style="font-size: 12px; text-align: left">
                       <td ><?php echo utf8_encode(addslashes($supervisor["supervisor"]))?></td>
                       <td ><?php echo utf8_encode(addslashes($supervisor["cuenta"]))?></td>
                       <td ><?php echo $supervisor["pos_total"]?></td>
                       <td ><?php echo $supervisor["ganadores_gc"]?></td>
-                      <td ><?php echo $supervisor["locales_ganador_gc"]?></td>
+                      <td ><?php echo $supervisor["locales_ganador_gc"]?> %</td>
                       <td ><?php echo $supervisor["gana"]?></td>
                     </tr>
 
