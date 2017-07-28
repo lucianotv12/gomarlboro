48dd7af3d94ebd91e9f1edaa719345b8a83a9668
Ibris <lucianotv12@gmail.com>
2017-07-28 15:14:45 -0300

    D

--- view/ranking-locales-a-2.php
+++ view/ranking-locales-a-2.php
@@ -9,8 +9,8 @@
               <tr><td colspan="8" style="text-align: center;">ACUMULADO SEMANA 1</td></tr>
 
                 <tr>
-                  <td width="140px" >Supervisor</td>
-                  <td width="140px" >Direccion</td>
+                  <td width="120px" >Supervisor</td>
+                  <td width="120px" >Direccion</td>
                   <td>Puntaje <br/>OB.Codigos</td>
                   <td>Compras</td>
                   <td>OB.Compras</td>
@@ -40,8 +40,8 @@
               ?>     
        
                     <tr style="font-size: 12px; text-align: left;">
-                      <td style="text-align: center" width="140px" ><?php echo utf8_encode(addslashes($pdv["supervisor"]))?></td>
-                      <td style="text-align: center" width="140px"><?php echo utf8_encode(addslashes($pdv["direccion"]))?></td>
+                      <td style="text-align: center" width="120px" ><?php echo utf8_encode(addslashes($pdv["supervisor"]))?></td>
+                      <td style="text-align: center" width="120px"><?php echo utf8_encode(addslashes($pdv["direccion"]))?></td>
                       <td style="text-align: center" width="100px"><?php echo $pdv["cupones"]?></td>
                       <td style="text-align: center" width="100px"><?php echo $pdv["compras_mlb"]?></td>
                       <td style="text-align: center" width="100px"><?php echo $pdv["compras_ob"]?></td>
